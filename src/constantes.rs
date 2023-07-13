80b719eab8f1b758547d0e4cbd18a865adff341b
Gustavo Pinhal <gustavopirespinhal@gmail.com>
2023-07-13 10:16:39 -0400

    Adição de constantes.rs

---
+++ src/constantes.rs
@@ -0,0 +1,15 @@
+use piston_window::types::Color;
+
+//Constantes gameplay
+pub const PERIODO_MOVIMENTO: f64 = 0.20 ;
+pub const TEMPO_RESTART: f64 = 1.0; 
+
+//Cores
+pub const COR_COBRA: Color = [0.15, 0.35, 0.65, 1.0];
+pub const COR_FUNDO: Color = [0.08, 0.20, 0.32, 1.0];
+pub const COR_FRUTA: Color = [0.90, 0.76, 0.20, 1.0];
+pub const COR_BORDA: Color = [0.04, 0.10, 0.16, 1.0];
+pub const COR_GAMEOVER: Color = [0.90, 0.00, 0.00, 0.5];
+
+//Desenho
+pub const TAMANHO_BLOCO: f64 = 20.0;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate rand;
 extern crate piston_window;
 
+mod constantes;
+
 fn main() {
     println!("Hello, world!");
 }
